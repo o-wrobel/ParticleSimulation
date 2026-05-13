7d7693895b4d4716349dad8febd4fc00a2bd161c
Oskar Wróbel <o.wrobel2025@protonmail.com>
2026-05-13 17:59:04 +0200

    Remove redundant comments in build files

--- build.zig
+++ build.zig
@@ -1,36 +1,12 @@
 const std = @import("std");
 const rlz = @import("raylib_zig");
 
-// Although this function looks imperative, it does not perform the build
-// directly and instead it mutates the build graph (`b`) that will be then
-// executed by an external runner. The functions in `std.Build` implement a DSL
-// for defining build steps and express dependencies between them, allowing the
-// build runner to parallelize the build automatically (and the cache system to
-// know when a step doesn't need to be re-run).
 pub fn build(b: *std.Build) !void {
-	// This creates a top level step. Top level steps have a name and can be
-	// invoked by name when running `zig build` (e.g. `zig build run`).
-	// This will evaluate the `run` step rather than the default step.
-	// For a top level step to actually do something, it must depend on other
-	// steps (e.g. a Run step, as we will see in a moment).
-
-
 	// TODO: Maybe place this somewhere else. Emscripten definitions require this to be before them
 	const run_step = b.step("run", "Run the app");
 
-	// Standard target options allow the person running `zig build` to choose
-	// what target to build for. Here we do not override the defaults, which
-	// means any target is allowed, and the default is native. Other options
-	// for restricting supported target set are available.
 	const target = b.standardTargetOptions(.{});
-	// Standard optimization options allow the person running `zig build` to select
-	// between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-	// set a preferred release mode, allowing the user to decide how to optimize.
 	const optimize = b.standardOptimizeOption(.{});
-	// It's also possible to define more custom flags to toggle optional features
-	// of this build script using `b.option()`. All defined flags (including
-	// target and optimize options) will be listed when running `zig build --help`
-	// in this directory.
 
 	const lpe_dep = b.dependency("little_physics_engine", .{
 		.target = target,
@@ -49,14 +25,7 @@ pub fn build(b: *std.Build) !void {
 	const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
 
 	const exe_mod = b.createModule(.{
-		// b.createModule defines a new module just like b.addModule but,
-		// unlike b.addModule, it does not expose the module to consumers of
-		// this package, which is why in this case we don't have to give it a name.
 		.root_source_file = b.path("src/main.zig"),
-		// Target and optimization levels must be explicitly wired in when
-		// defining an executable or library (in the root module), and you
-		// can also hardcode a specific target for an executable or library
-		// if desireable (e.g. firmware for embedded devices).
 		.target = target,
 		.optimize = optimize,
 	});
@@ -116,22 +85,6 @@ pub fn build(b: *std.Build) !void {
 		zip_cmd.step.dependOn(b.getInstallStep());
 		zip_step.dependOn(&zip_cmd.step);
 	} else {
-		// Here we define an executable. An executable needs to have a root module
-		// which needs to expose a `main` function. While we could add a main function
-		// to the module defined above, it's sometimes preferable to split business
-		// logic and the CLI into two separate modules.
-		//
-		// If your goal is to create a Zig library for others to use, consider if
-		// it might benefit from also exposing a CLI tool. A parser library for a
-		// data serialization format could also bundle a CLI syntax checker, for example.
-		//
-		// If instead your goal is to create an executable, consider if users might
-		// be interested in also being able to embed the core functionality of your
-		// program in their own executable in order to avoid the overhead involved in
-		// subprocessing your CLI tool.
-		//
-		// If neither case applies to you, feel free to delete the declaration you
-		// don't need and to put everything under a single module.
 		const exe = b.addExecutable(.{
 			.name = "physics_test",
 			.root_module = exe_mod
